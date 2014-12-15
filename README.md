2af71c6b3302be58105cb188741bc367a66580cd
Alex Indigo <iam@alexindigo.com>
2014-12-15 00:11:29 -0800

    Refactored injection procedure.

--- README.md
+++ README.md
@@ -1,25 +1,122 @@
-# requirejs-define
+# global-define
 
 Creates global define function to mimic requirejs define in node.
-Using [AMDefine](https://npmjs.org/package/amdefine) under the hood.
+Using [amdefine](http://npmjs.org/package/amdefine) under the hood.
 
-## Example
+Allows to require modules relative to `basePath` and escape `../../../../..` mess.
+Combined with support for `paths` aliases, provides unified environment
+for modules shared between server and browser (isomorphic javascript).
+
+## Examples
+
+### Basic
+
+Include following line within entry point of your application,
+and it will automatically add global define function to all
+included modules/files.
 
 `index.js`:
 
 ```javascript
-require('requirejs-define')({basePath: __dirname});
+require('global-define')({basePath: __dirname});
 ```
 
 `module.js`:
 
 ```javascript
-// works both in node and on the client
+// works in node and in a browser
 define([
   'app/models/property',
   'app/collections/property_search'
 ], function(Property, PropertySearch)
 {
   // ...
+});
+```
+
+### Path aliases
+
+Usually its suggested for module id to reflect path to the file,
+but its not always the case, this is where paths aliases come to help.
+
+In the following example `some_module` has several components
+located within `lib` folder.
+
+`index.js`:
+
+```javascript
+require('global-define')(
+{
+  basePath: __dirname,
+  paths:
+  {
+    'some_module/': 'some_module/lib/'
+  }
+});
+```
+
+`node_modules/some_module/lib/component.js`:
+
+```javascript
+
+define('some_module/component', [
+  'some_module/another_component'
+], function(Dependency)
+{
+  // ...
+});
+```
+
+### Black List
+
+If certain modules don't play nice with global define on the server,
+its possible to exclude them from the world of happiness,
+by adding them to the black list. It supports glob patterns via [minimatch](http://npmjs.org/package/minimatch) module.
+
+Following example will add global define only to the files of the project,
+without affecting any dependencies.
+
+`index.js`:
+
+```javascript
+require('global-define')(
+{
+  basePath:  __dirname,
+  blackList: ['node_modules/**']
+});
+```
+
+### White List
+
+If you want to play safe, you can white list only certain files in the project.
+
+Following example will add global define only to the files in `app/shared` folder.
+
+`index.js`:
+
+```javascript
+require('global-define')(
+{
+  basePath:  __dirname,
+  whiteList: ['app/shared/**']
+});
+```
+
+### Expose AMDefine
+
+By default it creates only global `define` function, so UMD modules still opt-in for server-side way of doing things (some of them are using UMD not only for loading resources, but to choose between browser/server branches of the code).
+
+If you don't have such modules (or blacklisted them) and you need better `requirejs`/`amdefine` support,
+set `exposeAmdefine` to true, to expose `.amd` and `.require` properties of the amdefine module
+on per file basis.
+
+`index.js`:
+
+```javascript
+require('global-define')(
+{
+  basePath:  __dirname,
+  blackList: ['node_modules/sinon/'],
+  exposeAmdefine: true
 });
 ```
--- index.js
+++ index.js
@@ -2,25 +2,49 @@
  * Creates global define function to mimic requirejs define in node
  */
 
-var basePath
-  , paths
-  , originalRequireJS
-  , fs       = require('fs')
-  , path     = require('path')
-  , amdefine = require('amdefine')
+var originalRequireJS
+  , basePath // root directory of the project
+  , basePathRegexp // used for search-n-replace with black/white lists handling
+  , exposeAmdefine = false // flag for exposing `.amd` and `.require` properities of `amdefine`
+  , paths          = {} // paths aliases
+  , blackList      = [] // excludes patterns from global define
+  , whiteList      = [] // limits global define to patterns
+  , fs             = require('fs')
+  , path           = require('path')
+  , amdefine       = require('amdefine')
+  , minimatch      = require('minimatch')
   ;
 
-// add global define function
+// augment default js extension
 if (require.extensions['.js']._id != module.id)
 {
   // hijack .js handler to add module specific define function to global namespace
   originalRequireJS = require.extensions['.js'];
   require.extensions['.js'] = function customRequireHandler(requiredModule, filename)
   {
-    // create global define specific to the module
-    global.define = amdefineWorkaround(requiredModule);
+    var compiledModule
+      , originalDefine = global.define // preserve original global define
+      ;
 
-    return originalRequireJS(requiredModule, filename);
+    // create global define specific to the module
+    // but only if its whitelisted and not blacklisted
+    if (isWhitelisted(requiredModule.id) && !isBlacklisted(requiredModule.id))
+    {
+      global.define = amdefineWorkaround(requiredModule);
+    }
+    else
+    {
+      // reset global define
+      delete global.define;
+    }
+
+    // compile module as per normal workflow
+    compiledModule = originalRequireJS(requiredModule, filename);
+
+    // revert back global define
+    global.define = originalDefine;
+
+    return compiledModule;
   }
 
   // mark the thing
@@ -30,8 +54,22 @@ if (require.extensions['.js']._id != module.id)
 // export API function to update basePath
 module.exports = function(options)
 {
-  basePath = options.basePath || process.cwd();
-  paths    = options.paths || {};
+  basePath  = options.basePath || process.cwd();
+  paths     = options.paths || paths;
+  blackList = options.blackList || blackList;
+  whiteList = options.whiteList || whiteList;
+
+  // if flag provided override default value
+  if ('exposeAmdefine' in options)
+  {
+    exposeAmdefine = options.exposeAmdefine;
+  }
+
+  // construct basePath regexp
+  basePathRegexp = new RegExp(('^' + basePath + '/').replace('/', '\\/'));
+
+  // return define tailored to the requiring module
+  return amdefineWorkaround(module.parent || process.mainModule);
 }
 
 // create workaround for amdefine
@@ -57,8 +95,11 @@ function amdefineWorkaround(requiredModule)
   }
 
   // make it look like legit thing
-  wrapper.amd = define.amd;
-  wrapper.require = define.require;
+  if (exposeAmdefine)
+  {
+    wrapper.amd     = define.amd;
+    wrapper.require = define.require;
+  }
 
   return wrapper;
 }
@@ -111,4 +152,49 @@ function checkPath(id)
   }
 
   return id;
+}
+
+// checks blackList, using glob-like patterns
+function isBlacklisted(moduleId)
+{
+  var i;
+
+  // strip basePath
+  moduleId = moduleId.replace(basePathRegexp, '');
+
+  for (i=0; i<blackList.length; i++)
+  {
+    if (minimatch(moduleId, blackList[i]))
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// checks whiteList, using glob-like patterns
+// if white list is empty treat it as everything in white list
+function isWhitelisted(moduleId)
+{
+  var i;
+
+  // check if its empty
+  if (!whiteList.length)
+  {
+    return true;
+  }
+
+  // strip basePath
+  moduleId = moduleId.replace(basePathRegexp, '');
+
+  for (i=0; i<whiteList.length; i++)
+  {
+    if (minimatch(moduleId, whiteList[i]))
+    {
+      return true;
+    }
+  }
+
+  return false;
 }
